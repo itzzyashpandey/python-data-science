b67b1c8c6490638f668f00beb271e1adfdd7e6ca
Yash Pandey <yashpandey1272000@gmail.com>
2022-08-23 18:07:36 +0530

    .

--- Advanced/fun1.py
+++ Advanced/fun1.py
@@ -27,3 +27,21 @@ def cal_area(w,h):
 cal_area(10,30)
 cal_area(20,30)
 cal_area(40,30)
+
+
+#return value wale 
+
+def calc_area_v2(w,h):
+    area = w*h
+    return area
+
+print(calc_area_v2(10,2))
+print(calc_area_v2(3,5))
+
+#storing return value in a variable 
+ans= calc_area_v2(5,5)
+print(ans)
+
+#using return values in a expresion 
+ans=calc_area_v2(3,5)+ calc_area_v2(3,5)
+print(ans)
---
+++ Advanced/g1.py
@@ -0,0 +1,23 @@
+from turtle import Screen, color
+import pgzrun
+
+WIDTH= 500
+HEIGHT=500
+
+box = Rect((50,150), (50,100))
+box2=Rect((105,150), (50,50))
+
+def draw():
+    screen.fill('orange')
+    screen.draw.text('hola amigos', (50,50), color='white')
+    screen.draw.text('this is gameprogramming', (50,80), color='yellow', fontsize=50)
+    screen.draw.rect(box,color='white',)
+    screen.draw.filled_rect(box2,color='cyan')
+     screen.draw.text('🤩', (20,80))
+    
+    
+def update():
+    box.x+=1
+    box2.y+=1
+    
+pgzrun.go()
